62c1f488a8970a46d75cfe9bea8cae851da609b8
Domin0x <kozlowski.dominik.mail@gmail.com>
2020-09-01 21:54:02 +0200

    improve helper methods

--- src/main/java/com/domin0x/BREFScraper/mapping/ElementParser.java
+++ src/main/java/com/domin0x/BREFScraper/mapping/ElementParser.java
@@ -6,11 +6,25 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 public class ElementParser {
+    private static final String ZERO = "0";
+
     public static int getAsInt(Element element) {
-        return Integer.parseInt(element.text().replaceAll(",", ""));
+        return Integer.parseInt(replaceNullOrEmpty(element.text(), ZERO));
     }
-    public static BigDecimal getAsBigDecimal(Element element) {return new BigDecimal(element.text());}
+
+    public static BigDecimal getAsBigDecimal(Element element) {
+        return new BigDecimal(replaceNullOrEmpty(element.text(), ZERO));
+    }
+
     public static Optional<Element> getOptionalLinkElement(Element element){
         return Optional.ofNullable(element.selectFirst("a"));
+    }
+
+    private static String replaceNullOrEmpty(String value, String fallBack) {
+        return isNullOrEmpty(value) ? fallBack : value;
+    }
+
+    private static boolean isNullOrEmpty(String value) {
+        return value == null || value.isEmpty();
     }
 }
