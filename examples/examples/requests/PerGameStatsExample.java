abbeaa2a2f7a51745671271dc12c74d7b7cf359c
Domin0x <kozlowski.dominik.mail@gmail.com>
2020-09-05 13:54:44 +0200

    update example with writing to CSV

--- examples/examples/requests/PerGameStatsExample.java
+++ examples/examples/requests/PerGameStatsExample.java
@@ -1,22 +1,26 @@
 package examples.requests;
 
 import com.domin0x.BREFScraper.BasketballRefClient;
-import com.domin0x.BREFScraper.mapping.model.PerGameStats;
 import com.domin0x.BREFScraper.mapping.model.SeasonType;
 import com.domin0x.BREFScraper.request.PerGameStatsRequest;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class PerGameStatsExample {
-    public static void main(String [] args) throws IOException {
-        PerGameStatsRequest request = BasketballRefClient.getBaseStatsRequest(SeasonType.REGULAR_SEASON, 2020);
-        var stats= request.execute();
-
-        System.out.println(stats.size());
-        stats.stream()
-                .filter(s -> s.getTeam().getAbbreviation().equals("TOT"))
-                .map(PerGameStats::getTeam)
-                .forEach(System.out::println);
-
+    public static void main(String [] args) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+        try(Writer writer = new FileWriter("PerGameStats.csv", true)){
+            StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
+            for(int i = 1990; i<=2020; i++){
+                PerGameStatsRequest request = BasketballRefClient.getBaseStatsRequest(SeasonType.PLAYOFFS, i);
+                var stats= request.execute();
+                beanToCsv.write(stats);
+            }
+        }
     }
 }
